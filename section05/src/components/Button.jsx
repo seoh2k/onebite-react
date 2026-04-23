7e1c99ecea8d2f412ddf114e9e39baef779b64a5
seoh2k <seoh2k@gmail.com>
2026-04-23 21:36:52 +0900

    이벤트 처리

--- section05/src/components/Button.jsx
+++ section05/src/components/Button.jsx
@@ -1,6 +1,16 @@
 const Button = ({ children, text, color = "black"}) => {
-   return (
-        <button style={{ color: color }}>
+    // 이벤트 객체
+    const onClickButton = (e) => {
+        console.log(e);
+        console.log(text);
+    };
+    
+    return (
+        <button 
+            onClick={onClickButton}
+            // onMouseEnter={onClickButton}
+            style={{ color: color }}
+        >
             {text} - {color.toUpperCase()}
             {children}
         </button>
