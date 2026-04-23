cd57351c326af84abcc81ba436be5abed644551b
seoh2k <seoh2k@gmail.com>
2026-04-23 22:14:45 +0900

    State로 상태관리

--- section05/src/App.jsx
+++ section05/src/App.jsx
@@ -1,28 +1,28 @@
 import './App.css'
-import Header from './components/Header'
-import Main from './components/main'
-import Footer from './components/Footer'
-import Button from './components/Button'
+import { useState } from 'react';
 
 function App() {
-
-  const buttonProps = {
-    text: "메일",
-    color: "red",
-    a: 1,
-    b: 2,
-    c: 3
-  };
+  const [count, setState] = useState(0);
+  const [light, setLight] = useState("OFF");
 
   return (
     <>
-      <Button {...buttonProps} />
-      <Button text={"카페"} />
-      <Button text={"블로그"}>
-        <Header />
-      </Button>
+      <div>
+        <h1>{light}</h1>
+        <button onClick={() => {
+          setLight(light === "ON" ? "OFF" : "ON");
+        }}>
+          {light === "ON" ? "끄기" : "켜기"}
+        </button>
+      </div>
+      <h1>{count}</h1>
+      <button onClick={() => {
+        setState(count + 1);
+      }}>
+        +
+      </button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
