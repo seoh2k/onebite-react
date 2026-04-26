4b8a079d2cd18ff823a33458c81d4fcffe44e09e
seoh2k <seoh2k@gmail.com>
2026-04-26 09:44:52 +0900

    State로 사용자 입력 관리 - 긴딘한 회원가입 폼 작성

--- section05/src/App.jsx
+++ section05/src/App.jsx
@@ -1,15 +1,12 @@
-import './App.css'
-import { useState } from 'react';
+import './App.css';
 
-import Bulb from './components/Bulb';
-import Counter from './components/Counter';
+import Register from './components/Register';
 
 function App() {
 
   return (
     <>
-      <Bulb />
-      <Counter />
+      <Register />
     </>
   );
 }
