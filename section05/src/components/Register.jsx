65524f9d88c32fc70a4e934a7d474a8857f9af71
seoh2k <seoh2k@gmail.com>
2026-04-27 22:00:36 +0900

    useRef로 컴포넌트의 변수 생성 - useRef란 새로운 Referance객체를 생성하는 기능 - 값이 변경되더라도 컴포넌트를 리랜더링 시키지 않음

--- section05/src/components/Register.jsx
+++ section05/src/components/Register.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 
 // 간단한 회원가입 폼
 // 1. 이름
@@ -15,17 +15,30 @@ const Register = () => {
         bio: ""
     });
 
+    const countRef = useRef(0);
+    const inputRef = useRef();
+
     const onChange = (e) => {
+        countRef.current++;
+        console.log(countRef.current);
         setInput({
             ...input,
             [e.target.name] : e.target.value
         });
     };
 
+    const onSubmit = () => {
+        if(input.name === "") {
+            // 이름을 입력하는 DOM 요소 포커스
+            inputRef.current.focus();
+        }
+    };
+
     return ( 
         <div>
             <div>
                 <input 
+                    ref={inputRef}
                     name="name"
                     value= {input.name}
                     onChange={onChange} 
@@ -62,6 +75,8 @@ const Register = () => {
                     onChange={onChange}
                 />
             </div>
+
+            <button onClick={onSubmit}>제출</button>
         </div> 
     );
 };
