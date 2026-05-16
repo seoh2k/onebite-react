fc469b88959a126ac1422003b5443298d1bf01f3
seoh2k <seoh2k@gmail.com>
2026-05-16 14:22:02 +0900

    TodoList 기능 구현 준비

--- section08/src/App.jsx
+++ section08/src/App.jsx
@@ -1,9 +1,34 @@
 import './App.css';
+import { useState } from 'react';
 import Header from './assets/components/Header';
 import Editor from './assets/components/Editor';
 import List from './assets/components/List';
 
+const mockData = [
+    {
+      id: 0,
+      idDone : false,
+      content: "React 공부하기",
+      date: new Date().getTime,
+    },
+    {
+      id: 1,
+      idDone : false,
+      content: "빨래하기",
+      date: new Date().getTime,
+    },
+    {
+      id: 2,
+      idDone : false,
+      content: "노래 연습하기",
+      date: new Date().getTime,
+    }
+  ];
+
 function App() {
+  
+  const [todos, setTodos] = useState(mockData);
+
   return (
     <div className='App'>
       <Header />
