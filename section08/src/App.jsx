172a8966fad84f44d40be73f3e966f01ab31bfec
seoh2k <seoh2k@gmail.com>
2026-05-17 07:38:25 +0900

    Create 투두 추가하기

--- section08/src/App.jsx
+++ section08/src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css';
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import Header from './assets/components/Header';
 import Editor from './assets/components/Editor';
 import List from './assets/components/List';
@@ -9,30 +9,42 @@ const mockData = [
       id: 0,
       idDone : false,
       content: "React 공부하기",
-      date: new Date().getTime,
+      date: new Date().getTime(),
     },
     {
       id: 1,
       idDone : false,
       content: "빨래하기",
-      date: new Date().getTime,
+      date: new Date().getTime(),
     },
     {
       id: 2,
       idDone : false,
       content: "노래 연습하기",
-      date: new Date().getTime,
+      date: new Date().getTime(),
     }
   ];
 
 function App() {
   
   const [todos, setTodos] = useState(mockData);
+  const idRef = useRef(3);
+
+  const onCreate = (content) => {
+    const newTodo = {
+      id: idRef.current++,
+      isDone: false,
+      content: content,
+      date: new Date().getTime()
+    }
+
+    setTodos([newTodo, ...todos]);
+  }
 
   return (
     <div className='App'>
       <Header />
-      <Editor />
+      <Editor onCreate={onCreate}/>
       <List />
     </div>
   );
