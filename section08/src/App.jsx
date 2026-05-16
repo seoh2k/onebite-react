b232d948a5b39661d11d9b05e980d95706544e29
seoh2k <seoh2k@gmail.com>
2026-05-16 14:16:04 +0900

    TodoList UI 구현

--- section08/src/App.jsx
+++ section08/src/App.jsx
@@ -1,7 +1,16 @@
 import './App.css';
+import Header from './assets/components/Header';
+import Editor from './assets/components/Editor';
+import List from './assets/components/List';
 
 function App() {
-  return <>TodoList</>;
+  return (
+    <div className='App'>
+      <Header />
+      <Editor />
+      <List />
+    </div>
+  );
 }
 
 export default App;
